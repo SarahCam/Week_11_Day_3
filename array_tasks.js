dc6bbf336f2e8fe5a23bde7c2f9e2ca884dcf4ff
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-04 16:05:53 +0100

    concat

--- array_tasks.js
+++ array_tasks.js
@@ -1,35 +1,38 @@
 var arrayTasks = {
 
-	// concat: function (arr1, arr2) {
-
-	// },
+	concat: function (arr1, arr2) {
+		for(let element of arr2){
+			arr1.push(element);
+		}
+		return arr1;
+	},
 
 	// insertAt: function (arr, itemToAdd, index) {
-		
+
 	// },
 
 	// square: function (arr) {
-		
+
 	// },
 
 	// sum: function (arr) {
-		
+
 	// },
 
 	// findDuplicates: function (arr) {
-		
+
 	// },
 
 	// removeAndClone: function (arr, valueToRemove) {
-		
+
 	// },
 
 	// findIndexesOf: function (arr, itemToFind) {
-		
+
 	// },
 
 	// sumOfAllEvenNumbersSquared: function (arr) {
-		
+
 	// }
 
 }
